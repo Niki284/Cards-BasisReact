67f4fef6d500dea9c5d178e120c93b5a441c5d04
Nikita Dymovich <89484300+Niki284@users.noreply.github.com>
2022-05-06 11:09:23 +0200

    Add files via upload

---
+++ storage/framework/views/f202db9ddacd8c6caf7c5d4946d4be2abcc7351e.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<main>
+<video width="400" height="300" controls="controls" poster="video/duel.jpg">
+   <source src="https://www.youtube.com/watch?v=0AIjZJ2vqlM" type='video/ogg; codecs="theora, vorbis"'>
+   <source src="video/duel.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+   <source src="video/duel.webm" type='video/webm; codecs="vp8, vorbis"'>
+   Тег video не поддерживается вашим браузером. 
+   <a href="video/duel.mp4">Скачайте видео</a>.
+  </video>
+
+</main>
+
+</body>
+</html><?php /**PATH C:\Users\nikit\OneDrive\Bureaublad\laravel\oorbeeld-app\resources\views/about.blade.php ENDPATH**/ ?>
